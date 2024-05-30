17d825372bc0bfd161efd540a79a5b045797e643
Yunxin Liu <1999yunxin@gmail.com>
2024-05-29 18:45:34 -0700

    add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './assets/custom.css';
 
 const App = () => {
   return (
     <>
-    <h1>Hello World</h1>
+    <h1 className='text-primary'>Hello World</h1>
     </>
   )
 }
