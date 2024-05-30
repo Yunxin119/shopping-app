7930c7736fc36e57bdd47fcc39936f4eab4ae4fe
Yunxin Liu <1999yunxin@gmail.com>
2024-05-30 14:38:31 -0700

    switch href to linkContainer in head component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,31 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
 return (
     <header>
-    <Navbar className='bg-primary' expand='lg' collapseOnSelect>
+    <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-        <Navbar.Brand href='/' className='text-secondary'>Yunxin's</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-            <Nav.Link href='/cart'  className='text-secondary'>
-                <FaShoppingCart /> Cart
-            </Nav.Link>
-            <Nav.Link href='/login' className='text-secondary'>
-                <FaUser /> Log in
-            </Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
+            <LinkContainer to='/'>
+                <Navbar.Brand>Yunxin's</Navbar.Brand>
+            </LinkContainer>
+            
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className='ms-auto'>
+                    <LinkContainer to='/cart'>
+                        <Nav.Link >
+                            <FaShoppingCart /> Cart
+                        </Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/login'>
+                        <Nav.Link>
+                            <FaUser /> Log in
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav>
+            </Navbar.Collapse>
         </Container>
     </Navbar>
     </header>
