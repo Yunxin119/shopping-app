cf35a48de4e0f709bd70797ab314b9487a04a1d6
Yunxin Liu <1999yunxin@gmail.com>
2024-06-03 16:00:20 -0700

    asyncHandler in ProductRoutes.js. Using data in mongodb instead of using local data.

--- server/routes/ProductRoutes.js
+++ server/routes/ProductRoutes.js
@@ -1,17 +1,25 @@
 import express from 'express';
 const router = express.Router();
-import products from '../data/products.js';
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/ProductSchema.js'
 
 // GET: Products Index
-router.get('/', (req, res) => {
-    res.json(products)
-})
+
+router.get('/', asyncHandler(async (req, res) => {
+      const products = await Product.find({});
+      res.json(products);
+    }));
 
 // GET: Product Detail
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
-    const product = products.find((p)=> p._id === id);
-    res.json(product)
-})
 
+router.get(
+    '/:id',
+    asyncHandler(async (req, res) => {
+      const product = await Product.findById(req.params.id);
+      if (product) {
+        return res.json(product);
+      }
+      res.status(404).json({ message: 'Product not found' });
+    })
+  );
 export default router;
