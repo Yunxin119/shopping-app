39d07a30b8552a6e9762621e429f811d0154d1f4
Yunxin Liu <1999yunxin@gmail.com>
2024-06-03 17:39:42 -0700

    Product Controller Added. Product Routes cleanup

--- server/routes/ProductRoutes.js
+++ server/routes/ProductRoutes.js
@@ -1,26 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/ProductSchema.js'
+import { getProducts, getProductById } from '../controllers/ProductController.js';
 
-// GET: Products Index
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
-router.get('/', asyncHandler(async (req, res) => {
-      const products = await Product.find({});
-      res.json(products);
-    }));
-
-// GET: Product Detail
-
-router.get(
-    '/:id',
-    asyncHandler(async (req, res) => {
-      const product = await Product.findById(req.params.id);
-      if (product) {
-        return res.json(product);
-      }
-      res.status(404);
-      throw new Error('Resource Not Found');
-    })
-  );
 export default router;
