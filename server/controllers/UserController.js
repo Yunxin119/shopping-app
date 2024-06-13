cafd982549e678449658505f3bce734354768338
Yunxin Liu <1999yunxin@gmail.com>
2024-06-12 17:19:42 -0700

    Protect and admin verification, also corrected token error in user controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -13,18 +13,19 @@ import jwt from 'jsonwebtoken'
       throw new Error('Please provide both email and password');
     }
 
-    //Set JWT as HTTP-only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30*24*60*60*1000 // 30 days
-    })
+
   
     const user = await User.findOne({ email });
   
     if (user && (await user.matchPassword(password))) {
       const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '30d'});
+      //Set JWT as HTTP-only cookie
+      res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 30*24*60*60*1000 // 30 days
+    })
       res.json(user.toObject()); // Use .toObject() to convert Mongoose document to plain JS object
     } else {
       res.status(401);
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import { authUser, registerUser, getAllUser, getUserById, updateUserById, getUserProfile, updateUserProfile, deleteUser } from '../controllers/UserController.js';
+import {protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getAllUser).post(registerUser);
+router.route('/').get(protect, admin, getAllUser).post(registerUser);
 router.route('/auth').post(authUser)
-router.route('/:id').get(getUserById).patch(updateUserById).delete(deleteUser);
-router.route('/profile').get(getUserProfile).patch(updateUserById)
+router.route('/:id').get(protect, admin,getUserById).patch(protect, admin,updateUserById).delete(protect, admin,deleteUser);
+router.route('/profile').get(protect,getUserProfile).patch(protect,updateUserById)
 
 export default router;
