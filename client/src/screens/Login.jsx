ee6675fade288c2bcc956bee20a39d8987a12dfc
Yunxin Liu <1999yunxin@gmail.com>
2024-06-13 16:29:39 -0700

    edit login screen

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,41 +1,88 @@
-import React from 'react'
-import { useState } from 'react'
-import {Link} from 'react-router-dom'
-import {Form, Row, Col, Button} from 'react-bootstrap'
-import FormContainer from '../components/FormContainer'
+import { useState, useEffect } from 'react';
+ import { Link, useLocation, useNavigate } from 'react-router-dom';
+ import { Form, Button, Row, Col } from 'react-bootstrap';
+ import { useDispatch, useSelector } from 'react-redux';
+ import Loader from '../components/Loader';
+ import FormContainer from '../components/FormContainer';
 
-const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
+ import { useLoginMutation } from '../slices/usersApiSlice';
+ import { setCredential } from '../slices/authSlice';
+ import { toast } from 'react-toastify';
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        console.log('submit')
-    }
-  return (
-    <FormContainer className='my-5'>
-      <h1>Sign In</h1>
-      <Form>
-        <Form.Group controlId='email' className='my-3'>
-            <Form.Label>Email</Form.Label>
-            <Form.Control type='email' placeholder='Enter your email here' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
-        </Form.Group>
+ const Login = () => {
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
 
-        <Form.Group controlId='password' className='my-3'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Enter your password here' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
-        </Form.Group>
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
 
-        <Button type='submit' variant='primary' className='my-3'>Sign In</Button>
-      </Form>
+   const [login, { isLoading }] = useLoginMutation();
 
-      <Row className='py-3'>
-        <Col>
-        New customer? <Link to='/register'>Register</Link>
-        </Col>
-      </Row>
-    </FormContainer>
-  )
-}
+   const { userInfo } = useSelector((state) => state.auth);
 
-export default Login
+   const { search } = useLocation();
+   const sp = new URLSearchParams(search);
+   const redirect = sp.get('redirect') || '/';
+
+   useEffect(() => {
+     if (userInfo) {
+       navigate(redirect);
+     }
+   }, [navigate, redirect, userInfo]);
+
+   const submitHandler = async (e) => {
+     e.preventDefault();
+     try {
+       const res = await login({ email, password }).unwrap();
+       dispatch(setCredential({ ...res }));
+       navigate(redirect);
+     } catch (error) {
+       toast.error(error?.data?.message || error.error);
+     }
+   };
+
+   return (
+     <FormContainer>
+       <h1>Sign In</h1>
+
+       <Form onSubmit={submitHandler}>
+         <Form.Group className='my-2' controlId='email'>
+           <Form.Label>Email</Form.Label>
+           <Form.Control
+             type='email'
+             placeholder='Enter email'
+             value={email}
+             onChange={(e) => setEmail(e.target.value)}
+           ></Form.Control>
+         </Form.Group>
+
+         <Form.Group className='my-2' controlId='password'>
+           <Form.Label>Password</Form.Label>
+           <Form.Control
+             type='password'
+             placeholder='Enter password'
+             value={password}
+             onChange={(e) => setPassword(e.target.value)}
+           ></Form.Control>
+         </Form.Group>
+
+         <Button disabled={isLoading} type='submit' variant='primary' className='my-3'>
+           Sign In
+         </Button>
+
+         {isLoading && <Loader />}
+       </Form>
+
+       <Row className='py-3'>
+         <Col>
+           New Customer?{' '}
+           <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+             Register
+           </Link>
+         </Col>
+       </Row>
+     </FormContainer>
+   );
+ };
+
+ export default Login;
