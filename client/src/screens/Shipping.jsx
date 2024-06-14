4401b28ef3b3a7ebc7276ded953749b30ff3cb1a
Yunxin Liu <1999yunxin@gmail.com>
2024-06-13 18:06:02 -0700

    checkout navigator component

--- client/src/screens/Shipping.jsx
+++ client/src/screens/Shipping.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../slices/cartSlice'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 const Shipping = () => {
 
@@ -25,6 +26,7 @@ const Shipping = () => {
     }
   return (
     <FormContainer>
+        <CheckoutSteps step1 step2 />
        <h1>Shipping</h1>
        <Form onSubmit={saveShippingAddressHandler}>
          <Form.Group className='my-2' controlId='address'>
