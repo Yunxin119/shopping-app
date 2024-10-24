8bef9399a202c5ca22ccf4458fd1b90e226b7e35
Yunxin Liu <1999yunxin@gmail.com>
2024-10-23 19:58:28 -0700

    attempt: deploy - dotenv not found

--- server/server.js
+++ server/server.js
@@ -39,17 +39,17 @@ app.use('/api/orders', OrderRoutes);
 app.use('/api/upload', UploadRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(path.resolve(), '/client/build')));
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', (req, res) => 
-        res.sendFile(path.resolve(path.resolve(), 'client', 'build', 'index.html'))
-    )
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 } else {
     app.get('/', (req, res) => {
         res.send('API is running');
-    })
-};
-
+    });
+}
 
 // use paypal in the app
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
