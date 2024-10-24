f716ee8b3f30aab8d9d1154062258854d20a53c3
Yunxin Liu <1999yunxin@gmail.com>
2024-10-23 20:11:46 -0700

    attempt2: port setting

--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ connect();
 
 const app = express();
 
-// const port = process.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
+// const port = 5000;
 
 //Home
 app.get('/', (req, res) => {
