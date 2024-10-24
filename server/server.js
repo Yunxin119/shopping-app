5666a74f7df1d6853d1762e0a08a2ca38ed7078f
Yunxin Liu <1999yunxin@gmail.com>
2024-10-23 20:19:44 -0700

    deploy error fixing

--- server/server.js
+++ server/server.js
@@ -19,11 +19,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 // const port = 5000;
 
-//Home
-app.get('/', (req, res) => {
-    res.send('API is running');
-})
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -39,11 +34,18 @@ app.use('/api/orders', OrderRoutes);
 app.use('/api/upload', UploadRoutes)
 
 const __dir = path.resolve();
-app.use(express.static(path.join(__dir, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dir, 'client/build')));
 
-app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dir, 'client', 'build', 'index.html'))
-);
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dir, 'client', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
+
 
 // use paypal in the app
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
